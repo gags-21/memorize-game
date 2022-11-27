98372d952b403c78090c4bc998c9edfd67662ce9
Gagan Bhirangi <gagan.bhirangi123@gmail.com>
2022-11-27 13:34:36 +0530

    MVVM Notification

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,19 +7,25 @@
 
 import SwiftUI
 
-class EmojiMemoryGame{
+class EmojiMemoryGame: ObservableObject {
     
     static var emojis = ["🚌","🚀","✈️","🚗","🚝","🚖","🚊","⛵️","🚅","🚟","🏍","🛵","🛻","🛺","🏎","🚒","🚛","🚜","🚁"]
     
     static func createMemoryGame() -> MemoryGame<String> {
         MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
-                emojis[pairIndex]
-            }
+            emojis[pairIndex]
+        }
     }
-
-    private var model: MemoryGame<String> = createMemoryGame()
+    
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose (_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
